5fc775751581e212fd1d241617f0cf664307ad94
n4cl <devn4cl@gmail.com>
2017-05-10 01:00:09 +0900

    Add group method in service.py

--- app/controller/service.py
+++ app/controller/service.py
@@ -1,13 +1,21 @@
 # coding: utf-8
 
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+from app.model.output import fetch_report, fetch_group_report
 
 app = Blueprint(__name__, "service")
 
-@app.route('/', methods=["GET", "POST"])
-def api():
+@app.route('/report', methods=["GET", "POST"])
+def group():
+    """ グループでの取得 """
     if request.method == "POST":
+        return "not implemented"
+        # TODO: エラートラップきちんとしよう
+
+        if request.headers['Content-Type'] != 'application/json':
+            return jsonify(res='error'), 500
         return 'POST'
+
     elif request.method == "GET":
-        return "GET"
+        return fetch_group_report()
 
