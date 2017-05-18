925a10752882543a8b9e74601a75e205425b8c0a
n4cl <devn4cl@gmail.com>
2017-05-19 01:31:09 +0900

    Add action to receive POST in service.py

--- app/controller/service.py
+++ app/controller/service.py
@@ -9,12 +9,17 @@ app = Blueprint(__name__, "service")
 def group():
     """ グループでの取得 """
     if request.method == "POST":
-        return "not implemented"
-        # TODO: エラートラップきちんとしよう
 
-        if request.headers['Content-Type'] != 'application/json':
+        if request.headers["Content-Type"] != "application/json":
             return jsonify(res='error'), 500
-        return 'POST'
+
+        # 日付データを受け取った場合、日付の日報を返す
+        req = request.json
+        if req:
+            if "date" in req:
+                return fetch_group_report(req["date"])
+
+        return "POST"
 
     elif request.method == "GET":
         return fetch_group_report()
