9e4b208ff7e9026ad7c9b97aa3fb32abe822c126
n4cl <devn4cl@gmail.com>
2018-09-10 17:36:14 +0900

    Fix domain in shell script

--- app/model/fetch_daily_report.sh
+++ app/model/fetch_daily_report.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 # 日報データを標準出力する
 set -eu
+source config
 
 # TODO: ファイルは保存しない、つどページの読み込みを行った方が良い
-wget -O - -q --no-check-certificate --load-cookies cookies.txt "https://3s.kksnet.co.jp/cgi-bin/sss/s3.cgi?$1" 
+wget -O - -q --no-check-certificate --load-cookies cookies.txt "https://$domain/cgi-bin/sss/s3.cgi?$1" 
 
--- app/model/fetch_report.sh
+++ app/model/fetch_report.sh
@@ -7,7 +7,7 @@ set -eu
 source ./config
 
 # ログインのためのクッキー更新
-wget -q --no-check-certificate --keep-session-cookies --save-cookies=cookies.txt -O page_cokkie --post-data "cmd=logincmd&UserID=$sss_user&_word=$sss_passwd&_submit=ログイン" https://3s.kksnet.co.jp/cgi-bin/sss/s3.cgi 
+wget -q --no-check-certificate --keep-session-cookies --save-cookies=cookies.txt -O page_cokkie --post-data "cmd=logincmd&UserID=$sss_user&_word=$sss_passwd&_submit=ログイン" https://$domain/cgi-bin/sss/s3.cgi 
 
 # 引数で入力した日付を取得
 d=$1
@@ -16,5 +16,5 @@ month="${d:4:2}"
 day="${d:6:2}"
 
 # 前日のデータを取得している
-wget -q --no-check-certificate --load-cookies cookies.txt -O - --post-data "cmd=schgrpweek&sid=0&nid=0&date=$year$month$day&bpage=schgrpweek&gid=10&bdate=$year$month$day&row=0&dspknd=0&uid=50&seldate=$year$month$day" https://3s.kksnet.co.jp/cgi-bin/sss/s3.cgi
+wget -q --no-check-certificate --load-cookies cookies.txt -O - --post-data "cmd=schgrpweek&sid=0&nid=0&date=$year$month$day&bpage=schgrpweek&gid=10&bdate=$year$month$day&row=0&dspknd=0&uid=50&seldate=$year$month$day" https://$domain/cgi-bin/sss/s3.cgi
 
