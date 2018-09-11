5d64fd197e6a249ee14482f0895bf39c52cadf9e
n4cl <devn4cl@gmail.com>
2018-09-11 16:44:20 +0900

    Add search.py

---
+++ app/model/search.py
@@ -0,0 +1,55 @@
+# coding: utf8
+
+import requests
+from json import dumps
+from ConfigParser import SafeConfigParser
+
+config = SafeConfigParser()
+config.read("app/model/config.ini")
+
+
+class ElasticSearchClient(object):
+
+    def __init__(self):
+        self.url = "http://" + config.get("elasticsearch", "ip")
+
+    def search_nippo(self, json):
+        search_api = self.url + "/es/_search"
+        json = dumps(json, encoding='utf-8')
+        return self._post_data(search_api, json)
+
+    def analyze_text(self, text):
+        analyze_api = self.url + "/es/_analyze?analyzer=ja_text_analyzer"
+        return self._post_data(analyze_api, text)
+
+    def _post_data(self, api, data):
+        res = requests.post(api, data=data)
+        return res.json()
+
+def main():
+    """ 動作テスト """
+    esc = ElasticSearchClient()
+
+    keyword = "CMS"
+    query = {
+             "query":{
+               "bool":{
+                 "must":[
+                   {
+                     "query_string":{
+                       "default_field":"text",
+                       "query": keyword,
+                     }
+                   },
+                 ],
+                }
+              }
+            }
+    print esc.search_nippo(query)
+
+    text = "日報のデータをダウンロードしました。"
+    print esc.analyze_text(text)
+
+
+if __name__ == '__main__':
+    main()
